c64d047dd66ba81a621d0aad3fed352ba4428431
Guilherme Estevan <guilhermeestevangui@hotmail.com>
2025-08-29 16:55:44 -0300

    ajustes mock

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,33 +1,33 @@
 "use client";
 
-import { useAuthStore } from "@/stores/auth-stores";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useAuthStore } from "@/stores/auth-stores";
+// // import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 import Layout from "@/components/layout/Layout";
-import LoadingSpinner from "@/components/spinners/LoadingSpinner";
-import Cookies from "js-cookie";
+// import LoadingSpinner from "@/components/spinners/LoadingSpinner";
+// import Cookies from "js-cookie";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { user } = useAuthStore();
-  const router = useRouter();
+  // const { user } = useAuthStore();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const token = Cookies.get("token");
+  // // useEffect(() => {
+  // //   const storedUser = localStorage.getItem("user");
+  // //   const token = Cookies.get("token");
 
-    if (!storedUser || !token) {
-      router.replace("/login");
-    } else {
-      useAuthStore.setState({ user: JSON.parse(storedUser) });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // //   if (!storedUser || !token) {
+  // //     router.replace("/login");
+  // //   } else {
+  // //     useAuthStore.setState({ user: JSON.parse(storedUser) });
+  // //   }
+  // //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // // }, []);
 
-  if (!user) return <LoadingSpinner />;
+  // if (!user) return <LoadingSpinner />;
 
   return <Layout>{children}</Layout>;
 }
